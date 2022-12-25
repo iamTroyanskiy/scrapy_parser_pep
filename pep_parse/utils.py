3f7d384863a53a9bc3fbedd4b43e762a0b3d5979
Anton Yaitskiy <tonik350@yandex.ru>
2022-12-25 22:38:37 +0700

    first try

--- pep_parse/utils.py
+++ pep_parse/utils.py
@@ -6,8 +6,8 @@ def dict_to_csv(file_path, dict_data, head):
     file.extend(dict_data.items())
     file.append(('Total', sum(dict_data.values())))
     try:
-        with open(file_path, mode='w', newline='', encoding='utf-8') as csv_file:
-            writer = csv.writer(csv_file)
+        with open(file_path, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
             writer.writerows(file)
     except IOError as error:
         print(str(error))
